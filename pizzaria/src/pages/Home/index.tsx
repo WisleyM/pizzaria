244836a71f9dd892d53f3874ce41e9d234675245
Wisley Mozer <156000537+WisleyM@users.noreply.github.com>
2024-03-12 19:57:18 +0000

    update 12/03

--- pizzaria/src/pages/Home/index.tsx
+++ pizzaria/src/pages/Home/index.tsx
@@ -1,8 +1,30 @@
+<import {ReactComponent as search } from "assets/icons/search.svg";  
 import * as S from "./style";
 const Home = () => {
   return(
     <S.Home>  
-    
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+          <div>
+            <S.HomeHeaderDetailsLogo>Pizza Fresh</HomeHeaderDetailsLogo>
+            <S.HomeHeaderDetailsDate>Aqui ficará a data</S.HomeHeaderDetailsDate>  
+          </div>
+            <S.HomeHeaderDetailsSearch>
+              <input type="text" placeholder="Procure pelo sabor" />
+              <Search />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
     </S.Home> 
   );
 }
